d334c84a01a05673e56e519fc522fda99eda226a
aronh <aronh@siminn.is>
2019-05-14 14:47:59 +0000

    * Change the URL

--- eurovision_data/eurovision_data/urls.py
+++ eurovision_data/eurovision_data/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path(r'video_data/', include('video_data.urls'))
+    #path(r'video_data/', include('video_data.urls'))
+    path(r'eurovision/', include('video_data.urls'))
 ]
