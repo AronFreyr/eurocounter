01c55c99830cb4dcb5e845238439eca598a48ac4
aronf <aronfreyrh@gmail.com>
2021-05-11 18:22:20 +0000

    Adding gunicorn to requirements

--- eurovision_data/video_data/eurovision_youtube_counter/run_cron.sh
+++ eurovision_data/video_data/eurovision_youtube_counter/run_cron.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
-
-/usr/local/bin/python3 /home/pi/git/euro_counter/eurovision_data/video_data/eurovision_youtube_counter/run_cron.py &>> /tmp/bash_cron_log.txt
-return
+source /opt/euro_counter/euro_counter_env/bin/activate
+python /opt/euro_counter/eurovision_data/video_data/eurovision_youtube_counter/run_cron.py & >> /tmp/bash_cron_log.txt
