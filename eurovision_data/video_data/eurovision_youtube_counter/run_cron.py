3c198bff8383620b2311b10a7bc97a548ee6285c
aronf <aronfreyrh@gmail.com>
2022-05-10 21:22:47 +0200

    Basic updates for 2022

--- eurovision_data/video_data/eurovision_youtube_counter/run_cron.py
+++ eurovision_data/video_data/eurovision_youtube_counter/run_cron.py
@@ -17,8 +17,8 @@ def run_cron():
     youtube = YoutubeConnect(config_location=conf_location)
     for video_groups in data['video_group']:
         link_list = [video_info['link'] for video_info in video_groups['videos']
-                     if video_groups['type'] == 'Euro semi finals 1 2021'
-                     or video_groups['type'] == 'Euro semi finals 2 2021']
+                     if video_groups['type'] == 'Euro semi finals 1 2022'
+                     or video_groups['type'] == 'Euro semi finals 2 2022']
         video_dict = youtube.get_video_data_for_video_list(link_list, video_groups['type'])
         db.store_data(video_dict)
         print('data storage successful')
